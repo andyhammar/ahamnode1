1121d7a83ccb23913aadd04f6a7d4625625cb1a9
Andreas Hammar <andreas.hammar@jayway.com>
2015-04-02 08:00:36 +0200

    not ending on line one

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var http = require('http')
 var port = process.env.PORT || 8081;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<html><body>');
+  res.write('<html><body>');
   
-  res.end('Hello World\n');
-  res.end('<h2>Awesome Azure!</h2>\n');
+  res.write('Hello World\n');
+  res.write('<h2>Awesome Azure!</h2>\n');
   res.end('</body></html>');
 }).listen(port);
