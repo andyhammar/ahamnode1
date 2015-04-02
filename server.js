5e629ee921244ccfb05a432cc9cd2a416ec69f72
Andreas Hammar <andreas.hammar@jayway.com>
2015-04-02 07:57:46 +0200

    html

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var http = require('http')
 var port = process.env.PORT || 8081;
 http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<html><body>');
+  
   res.end('Hello World\n');
+  res.end('<h2>Awesome Azure!</h2>\n');
+  res.end('</body></html>');
 }).listen(port);
